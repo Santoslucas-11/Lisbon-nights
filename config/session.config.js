64aa399eaf02e1b41629ee8455b08f220f7b2f41
Lucas Santos <lucasantos112291@gmail.com>
2023-01-10 17:57:55 +0000

    done

--- config/session.config.js
+++ config/session.config.js
@@ -1,8 +1,17 @@
 const session = require('express-session');
 const MongoStore = require("connect-mongo");
-const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/lisbon-nights-app";
 
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lisbon-nights-app";
+
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.wvpow0g.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 module.exports = app => {
     app.set('trust proxy', 1);
